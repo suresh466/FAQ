1beaf8ea51ca9ea64a1902cd25fceb564dee0b9f
Suresh <suresh.thagunna77@gmail.com>
2024-06-11 15:21:38 -0400

    create the database context

---
+++ FAQ/Models/FAQDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FAQ.Models
+{
+    public class FaqDbContext : DbContext
+    {
+        public FaqDbContext(DbContextOptions<FaqDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Question> Questions { get; set; }
+    }
+}
